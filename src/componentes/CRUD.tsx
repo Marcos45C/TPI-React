2e58a956c2bb2199ac103c1ddaa7194f28cedb52
MarcosC <marcos_45cerezo@hotmail.com>
2025-11-28 20:32:40 -0300

    botones

--- src/componentes/CRUD.tsx
+++ src/componentes/CRUD.tsx
@@ -174,18 +174,54 @@ export const CRUD = () => {
                     </td>
                     <td className="py-3 px-4 border-b">{cat.description}</td>
                     <td className="py-3 px-4 border-b text-center space-x-2">
-                      <button
+                      {/* <button
                         className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded text-sm transition"
                         onClick={() => EditarCategoria(cat.id)}
                       >
-                        Editar
+                        EditaRR
                       </button>
+
                       <button
                         className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition"
                         onClick={() => EliminarCategoria(cat.id)}
                       >
                         Eliminar
-                      </button>
+                      </button> */}
+                      <div className="inline-flex">
+                        <button className="-ml-px border border-gray-200 px-3 py-2 text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white focus:outline-none disabled:pointer-events-auto disabled:opacity-50">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="size-5"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                            ></path>
+                          </svg>
+                        </button>
+
+                        <button className="-ml-px rounded-r-sm border border-gray-200 px-3 py-2 text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900 focus:z-10 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-white focus:outline-none disabled:pointer-events-auto disabled:opacity-50">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            className="size-5"
+                          >
+                            <path
+                              stroke-linecap="round"
+                              stroke-linejoin="round"
+                              d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                            ></path>
+                          </svg>
+                        </button>
+                      </div>
                     </td>
                   </tr>
                 ))}
@@ -217,54 +253,54 @@ export const CRUD = () => {
           </button>
         </div>
         {openProducto && (
-        <div className="overflow-x-auto shadow rounded-lg">
-          <table className="min-w-full bg-white border border-gray-200 rounded-lg">
-            <thead className="bg-gray-100 text-gray-700 uppercase text-sm">
-              <tr>
-                <th className="py-3 px-4 border-b text-left">ID</th>
-                <th className="py-3 px-4 border-b text-left">TITULO</th>
-                <th className="py-3 px-4 border-b text-left">
-                  CATEGORIA QUE PERTENECEN
-                </th>
-                <th className="py-3 px-4 border-b text-left">Precio</th>
-                <th className="py-3 px-4 border-b text-center">ACCIONE</th>
-              </tr>
-            </thead>
-            <tbody>
-              {productoss.map((prod) => (
-                <tr
-                  key={prod.id}
-                  className="hover:bg-gray-50 transition text-gray-800 "
-                >
-                  <td className="py-3 px-4 border-b">{prod.id}</td>
-                  <td className="py-3 px-4 border-b font-semibold">
-                    {prod.title}
-                  </td>
-                  <td className="py-3 px-4 border-b">
-                    {prod.categoria_id || "sin categoría"}
-                  </td>
-                  <td className="py-3 px-4 border-b text-green-600 font-medium">
-                    ${prod.price ?? "N/A"}
-                  </td>
-                  <td className="py-3 px-4 border-b text-center space-x-2">
-                    <button
-                      className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded text-sm transition"
-                      onClick={() => EditarProducto(prod.id)}
-                    >
-                      Editar
-                    </button>
-                    <button
-                      className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition"
-                      onClick={() => EliminarProducto(prod.id)}
-                    >
-                      Eliminar
-                    </button>
-                  </td>
+          <div className="overflow-x-auto shadow rounded-lg">
+            <table className="min-w-full bg-white border border-gray-200 rounded-lg">
+              <thead className="bg-gray-100 text-gray-700 uppercase text-sm">
+                <tr>
+                  <th className="py-3 px-4 border-b text-left">ID</th>
+                  <th className="py-3 px-4 border-b text-left">TITULO</th>
+                  <th className="py-3 px-4 border-b text-left">
+                    CATEGORIA QUE PERTENECEN
+                  </th>
+                  <th className="py-3 px-4 border-b text-left">Precio</th>
+                  <th className="py-3 px-4 border-b text-center">ACCIONE</th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+              </thead>
+              <tbody>
+                {productoss.map((prod) => (
+                  <tr
+                    key={prod.id}
+                    className="hover:bg-gray-50 transition text-gray-800 "
+                  >
+                    <td className="py-3 px-4 border-b">{prod.id}</td>
+                    <td className="py-3 px-4 border-b font-semibold">
+                      {prod.title}
+                    </td>
+                    <td className="py-3 px-4 border-b">
+                      {prod.categoria_id || "sin categoría"}
+                    </td>
+                    <td className="py-3 px-4 border-b text-green-600 font-medium">
+                      ${prod.price ?? "N/A"}
+                    </td>
+                    <td className="py-3 px-4 border-b text-center space-x-2">
+                      <button
+                        className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded text-sm transition"
+                        onClick={() => EditarProducto(prod.id)}
+                      >
+                        Editar
+                      </button>
+                      <button
+                        className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition"
+                        onClick={() => EliminarProducto(prod.id)}
+                      >
+                        Eliminar
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         )}
       </section>
 
@@ -288,56 +324,56 @@ export const CRUD = () => {
             Nuevo Tag
           </button>
         </div>
-         {openTags && ( // si toca la tags se despliega todo esto
-        <div className="overflow-x-auto shadow rounded-lg">
-          <table className="min-w-full bg-white border border-gray-200 rounded-lg">
-            <thead className="bg-gray-100 text-gray-700 uppercase text-sm">
-              <tr>
-                <th className="py-3 px-4 border-b text-left">ID</th>
-                <th className="py-3 px-4 border-b text-left">TÍTULO</th>
-                <th className="py-3 px-4 border-b text-center">ACCIONES</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tags.length > 0 ? (
-                tags.map((tag) => (
-                  <tr
-                    key={tag.id}
-                    className="hover:bg-gray-50 transition text-gray-800"
-                  >
-                    <td className="py-3 px-4 border-b">{tag.id}</td>
-                    <td className="py-3 px-4 border-b font-semibold">
-                      {tag.title}
-                    </td>
-                    <td className="py-3 px-4 border-b text-center space-x-2">
-                      <button
-                        className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded text-sm transition"
-                        onClick={() => EditarTags(tag.id)}
-                      >
-                        Editar
-                      </button>
-                      <button
-                        className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition"
-                        onClick={() => EliminarTag(tag.id)}
-                      >
-                        Eliminar
-                      </button>
+        {openTags && ( // si toca la tags se despliega todo esto
+          <div className="overflow-x-auto shadow rounded-lg">
+            <table className="min-w-full bg-white border border-gray-200 rounded-lg">
+              <thead className="bg-gray-100 text-gray-700 uppercase text-sm">
+                <tr>
+                  <th className="py-3 px-4 border-b text-left">ID</th>
+                  <th className="py-3 px-4 border-b text-left">TÍTULO</th>
+                  <th className="py-3 px-4 border-b text-center">ACCIONES</th>
+                </tr>
+              </thead>
+              <tbody>
+                {tags.length > 0 ? (
+                  tags.map((tag) => (
+                    <tr
+                      key={tag.id}
+                      className="hover:bg-gray-50 transition text-gray-800"
+                    >
+                      <td className="py-3 px-4 border-b">{tag.id}</td>
+                      <td className="py-3 px-4 border-b font-semibold">
+                        {tag.title}
+                      </td>
+                      <td className="py-3 px-4 border-b text-center space-x-2">
+                        <button
+                          className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 rounded text-sm transition"
+                          onClick={() => EditarTags(tag.id)}
+                        >
+                          Editar
+                        </button>
+                        <button
+                          className="bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded text-sm transition"
+                          onClick={() => EliminarTag(tag.id)}
+                        >
+                          Eliminar
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                ) : (
+                  <tr>
+                    <td
+                      colSpan={3}
+                      className="py-4 text-center text-gray-500 italic border-b"
+                    >
+                      No hay tags disponibles.
                     </td>
                   </tr>
-                ))
-              ) : (
-                <tr>
-                  <td
-                    colSpan={3}
-                    className="py-4 text-center text-gray-500 italic border-b"
-                  >
-                    No hay tags disponibles.
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
+                )}
+              </tbody>
+            </table>
+          </div>
         )}
       </section>
     </div>
